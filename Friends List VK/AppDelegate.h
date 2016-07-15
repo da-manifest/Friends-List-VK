3324cfc497cbbd76375d02f2462d5b1450db4c4f
Admin <admin@Mac-Admin.local>
2016-07-15 21:22:57 +0500

    -начало проекта

---
+++ Friends List VK/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Friends List VK
+//
+//  Created by Admin on 15/07/16.
+//  Copyright © 2016 da_manifest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Friends List VK/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Friends List VK
+//
+//  Created by Admin on 15/07/16.
+//  Copyright © 2016 da_manifest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
